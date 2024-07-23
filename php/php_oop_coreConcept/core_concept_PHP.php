e8c209ae6be91a8c21c49eade50821b0f208873d
aungNyeinChan93 <aungNyeinChan.dev@gmail.com>
2024-07-23 10:34:53 +0630

    update

--- php/php_oop_coreConcept/core_concept_PHP.php
+++ php/php_oop_coreConcept/core_concept_PHP.php
@@ -22,6 +22,7 @@ if(!$test == 1 || $test <10){
     echo " true <br>";
 }
 
+echo "==============================<br>";
 // array in PHP
 $my_arrays = array("My string", 12, 0.2, true, null, "");
 $my_arrays[] = ["hello World!"];
@@ -29,14 +30,20 @@ $my_arrays[16] = new stdClass();
 $my_arrays[16]->name = "obj";
 $my_arrays[] = ["key" => "value", "properties" => ["a", "b", "c", "d"]];
 
+print "<pre>";
 $val = [];
 foreach ($my_arrays as $values) {
     echo gettype($values);
-    print "<pre>";
     var_dump($values);
     echo "<br>";
     $val[] = $values;
     for ($i = 0; $i < count($val); $i++) {
         print_r($val[$i]);
+        $v= [];
+        $v[] = $val[$i];
+        for($j = 0; $j< count($v);$j++){
+            var_dump($v[$j]);
+        }
     }
 }
+;

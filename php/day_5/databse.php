c8b2e58476ca2960c4818c9266ab3c6f7d91a1f8
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-08-04 13:04:41 +0630

    DB:: PDO connection

---
+++ php/day_5/databse.php
@@ -0,0 +1,14 @@
+<?php
+echo "<pre>";
+
+// var_dump($pdo);
+
+try {
+    //DB connection get::
+    $pdo = new PDO("mysql:host=127.0.0.1;dbname=cars", "root", "Anc@123"); //  <<--  :-)
+    echo "DB connection success!<br> ";
+} catch (PDOException $error) {
+    // echo "connection error..." . $error . " <br>";
+    $exp = new PDOException("DB::connection erros $error");
+    echo $exp->getMessage();
+}

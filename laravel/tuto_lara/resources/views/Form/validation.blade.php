53526d34252200572446b1dec5c2e072df1d46d8
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-08-23 08:19:58 +0630

    update

--- laravel/tuto_lara/resources/views/Form/validation.blade.php
+++ laravel/tuto_lara/resources/views/Form/validation.blade.php
@@ -21,31 +21,32 @@
             @endif
             <form action="{{route("validation")}}" class=" shadow-sm p-3 rounded " method="POST">
                 @csrf
-                <input type="text" name="name" class="form-control my-1  @error("name") is-invalid @enderror " placeholder="Enter Name...">
+                <input type="text" name="name" value="{{old("name")}}" class="form-control my-1  @error("name") is-invalid @enderror " placeholder="Enter Name...">
                 @error("name")
                     <small class=" d-block alert alert-warning">
                         {{$message}}
                     </small>
                 @enderror
-                <input type="email" name="email" class="form-control my-1  @error("email") is-invalid @enderror" placeholder="Enter Email...">
+                <input type="email" name="email" value="{{old("email")}}" class="form-control my-1  @error("email") is-invalid @enderror" placeholder="Enter Email...">
                 @error("email")
                     <small class=" d-block alert alert-warning">
                         {{$message}}
                     </small>
                 @enderror
-                <input type="password" name="password" class="form-control my-1 @error("password") is-invalid @enderror" placeholder="Enter Password...">
+                <input type="password" name="password" value="{{old("password")}}" class="form-control my-1 @error("password") is-invalid @enderror" placeholder="Enter Password...">
                 @error("password")
                     <small class=" d-block alert alert-warning">
                         {{$message}}
                     </small>
                 @enderror
-                <input type="password" name="passwordsec" class="form-control my-1 @error("passwordsec") is-invalid @enderror" placeholder="Confirm Password...">
+                <input type="password" name="passwordsec" value="{{old("passwordsec")}}" class="form-control my-1 @error("passwordsec") is-invalid @enderror" placeholder="Confirm Password...">
                 @error("passwordsec")
                     <small class=" d-block alert alert-warning">
                         {{$message}}
                     </small>
                 @enderror
                 <textarea name="message" cols="30" rows="10" class="form-control my-1 @error("message") is-invalid @enderror" placeholder="Message">
+                    {{old("message")}}
                 </textarea>
                 @error("message")
                     <small class=" d-block alert alert-warning">
@@ -54,25 +55,25 @@
                 @enderror
                 <select name="language" class="form-control my-1 @error("language") is-invalid @enderror">
                     <option value="">Select language</option>
-                    <option value="html">HTML</option>
-                    <option value="css">CSS</option>
-                    <option value="js">JS</option>
+                    <option value="html" {{old("language") == "html" ? "selected":""}}>HTML</option>
+                    <option value="css" {{old("language" )== "css" ? "selected":""}}>CSS</option>
+                    <option value="js" {{old("language") == "js" ? "selected":""}}>JS</option>
                 </select>
                 @error("language")
                     <small class=" d-block alert alert-warning">
                         {{$message}}
                     </small>
                 @enderror
-                <input type="radio" name="gender" value="male" class="form-check-input my-1"> Male
-                <input type="radio" name="gender" value="female" class="form-check-input my-1"> Female
+                <input type="radio" name="gender" value="male" {{old("gender") == "male"? "checked":"" }} class="form-check-input my-1"> Male
+                <input type="radio" name="gender" value="female" {{old("gender") == "female"? "checked":"" }} class="form-check-input my-1"> Female
                 @error("gender")
                     <small class=" d-block alert alert-warning">
                         {{$message}}
                     </small>
                 @enderror
                 <div class="check">
-                    <input type="checkbox" name="lvl" value="level_1" class="form-check-input my-1 "> level-1
-                    <input type="checkbox" name="lvl" value="level_2" class="form-check-input my-1 "> level-2
+                    <input type="checkbox" name="lvl" value="level_1" {{old("lvl") == "level_1" ? "checked" :""}} class="form-check-input my-1 "> level-1
+                    <input type="checkbox" name="lvl" value="level_2" {{old("lvl") == "level_2" ? "checked" :""}} class="form-check-input my-1 "> level-2
                 </div>
                 @error("lvl")
                     <small class=" d-block alert alert-warning">

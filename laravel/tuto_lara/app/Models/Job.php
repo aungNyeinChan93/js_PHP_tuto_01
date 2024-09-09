10b87f215d82facfcb38d0b946ffce2869ff4c26
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-09-10 00:29:32 +0630

    update

--- laravel/tuto_lara/app/Models/Job.php
+++ laravel/tuto_lara/app/Models/Job.php
@@ -10,4 +10,13 @@ class Job extends Model
     use HasFactory;
 
     protected $fillable = ["position","salary"];
+
+
+    public function employer(){
+        return $this->belongsTo(Employer::class,"employer_id");
+    }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
